f31517655d3deb2b3ff778374f005137828ce45d
Programir <zzradio@mail.ru>
2019-03-09 14:33:42 +0200

    WPF test

--- Chat/MainWindow.xaml.cs
+++ Chat/MainWindow.xaml.cs
@@ -15,14 +15,41 @@ using System.Windows.Shapes;
 
 namespace Chat
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void TextBox_Login(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            MessageBox.Show(textBox.Text);
+        }
+
+        private void Server_IP(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            MessageBox.Show(textBox.Text);
+        }
+
+        private void Server_Port(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            MessageBox.Show(textBox.Text);
+        }
+
+        private void Connect_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Кнопка соединения нажата");
+        }
+
+        private void Escape_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
     }
 }
